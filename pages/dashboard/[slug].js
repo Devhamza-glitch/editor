0259dad33364f2e665c6459ba533e4f41e2714fe
Muhammad Hamza <muhammad.hamzadev@codistan.org>
2022-10-29 01:26:43 +0500

    toolbar addedd

--- pages/dashboard/[slug].js
+++ pages/dashboard/[slug].js
@@ -7,12 +7,25 @@ import { FiSave, FiUnlock } from "react-icons/fi";
 import { GrSend } from "react-icons/gr";
 import { VscOpenPreview } from "react-icons/vsc";
 import { useEditor, EditorContent } from "@tiptap/react";
+import Underline from "@tiptap/extension-underline";
 import StarterKit from "@tiptap/starter-kit";
 import { useRouter } from "next/router";
+import {
+  FaBold,
+  FaHeading,
+  FaItalic,
+  FaListOl,
+  FaListUl,
+  FaQuoteLeft,
+  FaRedo,
+  FaStrikethrough,
+  FaUnderline,
+  FaUndo,
+} from "react-icons/fa";
 
 export async function getStaticProps(context) {
   let slug = context?.params?.slug;
-  console.log(slug)
+  console.log(slug);
   let data = [];
   await fetch(
     "http://localhost:3000/api/readFile",
@@ -48,8 +61,91 @@ const Dashboard = ({ data }) => {
   const [value, setValue] = useState(data);
   const router = useRouter();
 
+  const MenuBar = ({ editor }) => {
+    if (!editor) {
+      return null;
+    }
+  
+    return (
+      <div className="menuBar">
+        <div>
+          <button
+            onClick={() => editor.chain().focus().toggleBold().run()}
+            className={editor.isActive("bold") ? "is_active" : ""}
+          >
+            <FaBold />
+          </button>
+          <button
+            onClick={() => editor.chain().focus().toggleItalic().run()}
+            className={editor.isActive("italic") ? "is_active" : ""}
+          >
+            <FaItalic />
+          </button>
+          <button
+            onClick={() => editor.chain().focus().toggleUnderline().run()}
+            className={editor.isActive("underline") ? "is_active" : ""}
+          >
+            <FaUnderline />
+          </button>
+          <button
+            onClick={() => editor.chain().focus().toggleStrike().run()}
+            className={editor.isActive("strike") ? "is_active" : ""}
+          >
+            <FaStrikethrough />
+          </button>
+          <button
+            onClick={() =>
+              editor.chain().focus().toggleHeading({ level: 2 }).run()
+            }
+            className={
+              editor.isActive("heading", { level: 2 }) ? "is_active" : ""
+            }
+          >
+            <FaHeading />
+          </button>
+          <button
+            onClick={() =>
+              editor.chain().focus().toggleHeading({ level: 3 }).run()
+            }
+            className={
+              editor.isActive("heading", { level: 3 }) ? "is_active" : ""
+            }
+          >
+            <FaHeading className="heading3" />
+          </button>
+          <button
+            onClick={() => editor.chain().focus().toggleBulletList().run()}
+            className={editor.isActive("bulletList") ? "is_active" : ""}
+          >
+            <FaListUl />
+          </button>
+          <button
+            onClick={() => editor.chain().focus().toggleOrderedList().run()}
+            className={editor.isActive("orderedList") ? "is_active" : ""}
+          >
+            <FaListOl />
+          </button>
+          <button
+            onClick={() => editor.chain().focus().toggleBlockquote().run()}
+            className={editor.isActive("blockquote") ? "is_active" : ""}
+          >
+            <FaQuoteLeft />
+          </button>
+        </div>
+        <div>
+          <button onClick={() => editor.chain().focus().undo().run()}>
+            <FaUndo />
+          </button>
+          <button onClick={() => editor.chain().focus().redo().run()}>
+            <FaRedo />
+          </button>
+        </div>
+      </div>
+    );
+  };
+
   const editor = useEditor({
-    extensions: [StarterKit],
+    extensions: [StarterKit,Underline],
     onUpdate({ editor }) {
       setValue(editor.getHTML());
     },
@@ -139,6 +235,7 @@ const Dashboard = ({ data }) => {
             <Text style={{ color: "black" }}>Home Slides</Text>
           </div>
           <Wrapper visible={showContent}>
+            <MenuBar editor={editor} />
             <EditorContent editor={editor} />
           </Wrapper>
         </Content>
